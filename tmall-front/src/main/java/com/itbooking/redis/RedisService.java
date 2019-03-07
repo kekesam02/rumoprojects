006663c75fb535251811bfe56ba0a73b39f575b8
kekesam02 <1571828260@qq.com>
2019-03-07 20:36:14 +0800

    redisservice初始化提交

---
+++ tmall-front/src/main/java/com/itbooking/redis/RedisService.java
@@ -0,0 +1,15 @@
+package com.itbooking.redis;
+
+/**
+ * 
+ * 
+ * RedisService<br/>
+ * 创建人:xuchengfeifei<br/>
+ * 手机/微信:15074816437<br/> 
+ * 时间：2019年3月7日-下午8:35:42 <br/>
+ * @version 1.0.0<br/>
+ *
+ */
+public class RedisService {
+
+}
